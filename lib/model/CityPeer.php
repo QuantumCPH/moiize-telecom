68a1e2438f04f23f3eb01a89913e9c07dbe35111
Fatima Umer <fu@zerocall.com>
2012-03-07 13:09:45 +0500

    backend

--- lib/model/CityPeer.php
+++ lib/model/CityPeer.php
@@ -6,7 +6,7 @@ class CityPeer extends BaseCityPeer
 
     static public function getSortedSweedishCities() {
         $c = new Criteria();
-        $c->add(CityPeer::COUNTRY_ID,73);
+        $c->add(CityPeer::COUNTRY_ID,185);
         $c->addAscendingOrderByColumn(CityPeer::NAME);
         $rs = CityPeer::doSelect($c);
         return $rs;
