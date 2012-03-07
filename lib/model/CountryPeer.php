ef2b644c230a8fcdd9d71ece3b3661626586a9b8
Fatima Umer <fu@zerocall.com>
2012-03-07 12:58:08 +0500

    backend

--- lib/model/CountryPeer.php
+++ lib/model/CountryPeer.php
@@ -4,7 +4,7 @@ class CountryPeer extends BaseCountryPeer
 {
      static public function getSortedCountries() {
         $c = new Criteria();
-         $c->addAnd(CountryPeer::ID,73);
+         $c->addAnd(CountryPeer::ID,185);
         $c->addAscendingOrderByColumn(CountryPeer::NAME);
         $rs = CountryPeer::doSelect($c);
         return $rs;
