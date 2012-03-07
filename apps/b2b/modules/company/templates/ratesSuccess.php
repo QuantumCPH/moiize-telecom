5b0030afb2c2287fda7f80558c077f3906f8507c
Rubab Rafique <rr@zerocall.com>
2012-03-07 18:08:48 +0500

    rates view

---
+++ apps/b2b/modules/company/templates/ratesSuccess.php
@@ -0,0 +1,12 @@
+<?php use_helper('I18N') ?>
+<div id="sf_admin_container">
+    <h1><?php echo __('Rates') ?></h1>
+</div>  
+<div class="borderDiv">
+    <div id="sf_admin_content">
+        <div id="company-info">
+            
+                <div class="clr"></div>
+        </div>
+    </div>
+</div>
