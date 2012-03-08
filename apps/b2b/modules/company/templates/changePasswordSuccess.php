4d9a09102483aefb26af5d48e560f01867afbcc8
Rubab Rafique <rr@zerocall.com>
2012-03-08 12:20:24 +0500

    rates view, change password in agent, agent info, sms for forgot password for agent

---
+++ apps/b2b/modules/company/templates/changePasswordSuccess.php
@@ -0,0 +1,23 @@
+<div id="sf_admin_container"><h1><?php echo __('Change Password') ?></h1></div>
+
+<div class="borderDiv">
+    <br/>
+    <div>
+        <form action="<?php echo url_for(sfConfig::get('app_main_url').'company/changePassword') ?>" name="frmChangePassword" method="post">
+            <input type="hidden" value="<?php echo $vatNo;?>" name="vatNum" />
+            <table cellspacing="0" cellpadding="0" border="0">
+                <tr>
+                    <th>Old Password:</th><td><input type="text" value="" name="oldPassword" /></td>
+                </tr>
+                <tr>
+                    <th>New Password:</th><td><input type="text" value="" name="newPassword" /></td>
+                </tr>
+                <tr><td colspan="2" align="right"><input type="submit" name="submit" value="Change" /></td></tr>
+            </table>
+            </table>
+        </form>        
+        
+    </div>
+
+    <div class="clr"></div>
+</div>					
