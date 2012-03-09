c32d5d36af6a4d01f17fc616cd6c7a0fcbff4da5
Rubab Rafique <rr@zerocall.com>
2012-03-09 16:00:58 +0500

    logo in refill receipt

--- lib/emailLib.php
+++ lib/emailLib.php
@@ -1276,30 +1276,30 @@ WLS2<br/><a href='http://www.wls2.zerocall.com'>www.wls2.zerocall.com</a></td></
         endif;
         //---------------------------------------
         //--------------Sent The Email To okhan
-        if (trim($sender_email) != ''):
-            $email3 = new EmailQueue();
-            $email3->setSubject($subject);
-            $email3->setReceipientName($sender_name);
-            $email3->setReceipientEmail($sender_email);
-            $email3->setAgentId($referrer_id);
-            $email3->setCutomerId($customer_id);
-            $email3->setEmailType('Company Refill');
-            $email3->setMessage($message_body);
-            $email3->save();
-        endif;
-        //-----------------------------------------
-        //--------------Sent The Email To CDU
-        if (trim($sender_emailcdu) != ''):
-            $email4 = new EmailQueue();
-            $email4->setSubject($subject);
-            $email4->setReceipientName($sender_namecdu);
-            $email4->setReceipientEmail($sender_emailcdu);
-            $email4->setAgentId($referrer_id);
-            $email4->setCutomerId($customer_id);
-            $email4->setEmailType('Moiize Refill');
-            $email4->setMessage($message_body);
-            $email4->save();
-        endif;
+//        if (trim($sender_email) != ''):
+//            $email3 = new EmailQueue();
+//            $email3->setSubject($subject);
+//            $email3->setReceipientName($sender_name);
+//            $email3->setReceipientEmail($sender_email);
+//            $email3->setAgentId($referrer_id);
+//            $email3->setCutomerId($customer_id);
+//            $email3->setEmailType('Company Refill');
+//            $email3->setMessage($message_body);
+//            $email3->save();
+//        endif;
+//        //-----------------------------------------
+//        //--------------Sent The Email To CDU
+//        if (trim($sender_emailcdu) != ''):
+//            $email4 = new EmailQueue();
+//            $email4->setSubject($subject);
+//            $email4->setReceipientName($sender_namecdu);
+//            $email4->setReceipientEmail($sender_emailcdu);
+//            $email4->setAgentId($referrer_id);
+//            $email4->setCutomerId($customer_id);
+//            $email4->setEmailType('Moiize Refill');
+//            $email4->setMessage($message_body);
+//            $email4->save();
+//        endif;
         //-----------------------------------------
     }
 
