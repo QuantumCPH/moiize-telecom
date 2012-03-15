8da5d52b6c56eb1bf97c1777a139232b33e9ed8a
Baran Khan <bk@zapna.com>
2012-03-15 12:22:22 +0500

    URL Redirection

--- web/backend.php
+++ web/backend.php
@@ -1,4 +1,11 @@
 <?php
+var_dump($_SERVER);
+die;
+if($_SERVER['HTTP_HOST']!="localhost" &&$_SERVER['HTTP_HOST']=="moiize.com"&&$_SERVER['REQUEST_URI']=="backend.php" ){
+Header( "HTTP/1.1 301 Moved Permanently" );
+Header( "Location: http://admin.moiize.com" );
+
+}
 
 
 require_once(dirname(__FILE__).'/../config/ProjectConfiguration.class.php');
