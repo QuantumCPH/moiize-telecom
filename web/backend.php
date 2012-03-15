e59a9242959ef8ac67496a67288ecf8ca4e9ab34
Baran Khan <bk@zapna.com>
2012-03-15 12:30:26 +0500

    URL Redirection

--- web/backend.php
+++ web/backend.php
@@ -2,6 +2,8 @@
 echo $_SERVER['HTTP_HOST'];
 echo "<br/>".$_SERVER['REQUEST_URI']."<hr/>";
 if(($_SERVER['HTTP_HOST']=="moiize.com" || $_SERVER['HTTP_HOST']=="www.moiize.com") && $_SERVER['REQUEST_URI']=="/backend.php" ){
+
+die ("Khan");
 Header( "HTTP/1.1 301 Moved Permanently" );
 Header( "Location: http://admin.moiize.com" );
 }
