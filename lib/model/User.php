b0cb278963811e7db57741bc16fe928a6caeb202
Rubab Rafique <rr@zerocall.com>
2012-04-17 17:54:20 +0500

    user created time changed in admin

--- lib/model/User.php
+++ lib/model/User.php
@@ -5,4 +5,9 @@ class User extends BaseUser
     public function __toString(){
         return $this->getName();
     }
+    public function getCreateDate($format = 'Y-m-d H:i:s') {
+      // if($format=== null) $format = "Y-m-d H:i:s"; 
+       
+       return date("Y-m-d H:i:s", strtotime($this->getCreatedAt())+25200);
+    }
 }
