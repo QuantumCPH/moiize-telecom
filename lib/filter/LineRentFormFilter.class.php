2ba979b7cb627cd02b9a0e91d50046b23ea7dede
root <root@im.zerocall.com>
2012-04-25 06:53:14 -0400

    new line rent  module change

---
+++ lib/filter/LineRentFormFilter.class.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * LineRent filter form.
+ *
+ * @package    zapnacrm
+ * @subpackage filter
+ * @author     Your name here
+ */
+class LineRentFormFilter extends BaseLineRentFormFilter
+{
+  public function configure()
+  {
+  }
+}
