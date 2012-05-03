bee411f5cf0c659dfb8395b4ce410c9d38214d67
Fatima Umer <fu@zerocall.com>
2012-05-03 11:38:54 +0500

    edit agent module

--- lib/telintaSoap.class.php
+++ lib/telintaSoap.class.php
@@ -55,7 +55,6 @@ class PortaBillingSoapClient extends SoapClient {
                  $sessionId = $soap_client->login($login, $password);
               } catch (SoapFault $e) {
                     emailLib::sendErrorInTelinta("Login Issue","Could not Login with Billing Server. Error is " . $e->faultstring . "  <br/> Please Investigate.");
-                    $pb->_logout();
                     return false;
               }
                  $this->_setSessionId($sessionId);
