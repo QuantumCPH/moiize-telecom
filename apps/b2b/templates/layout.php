ee1a2381874d62ec5cc76c34e93a128836b9f859
Rubab Rafique <rr@zerocall.com>
2012-11-22 17:05:16 +0500

    logo

--- apps/b2b/templates/layout.php
+++ apps/b2b/templates/layout.php
@@ -10,7 +10,7 @@
 
     <body>
         <div id="basic">
-             <?php echo $_SERVER["HTTP_REFERER"]; if($_SERVER["HTTP_REFERER"]!="http://mw.zerocall.com/login/" || $_SERVER["HTTP_REFERER"]!="http://mw.zerocall.com/es/login/"):?>
+             <?php if($_SERVER["HTTP_REFERER"]!="http://mw.zerocall.com/login/" && $_SERVER["HTTP_REFERER"]!="http://mw.zerocall.com/es/login/"):?>
             <div id="header">
                 <div class="logo">
                     <?php echo image_tag('/images/logo.jpg'); // link_to(image_tag('/images/logo.gif'), '@homepage');  ?>
